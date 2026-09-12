b27a32dadb0f5720b07258e88a4737371b0c8029
HEMANT VERMA <hemantvermalakesher@gmail.com>
2026-09-12 11:17:33 +0530

    fix(types): annotate continuation serialization

--- src/pycontinuum/serialization.py
+++ src/pycontinuum/serialization.py
@@ -1,13 +1,17 @@
 """Serialization utilities for continuations."""
 
 import pickle
+from typing import Any, cast
 
 from .core import Continuation
 
 
-def dumps(cont: Continuation) -> bytes:
+def dumps(cont: Continuation[Any, Any]) -> bytes:
+    """Serialize a continuation using pickle."""
     return pickle.dumps(cont)
 
 
-def loads(data: bytes) -> Continuation:
-    return pickle.loads(data)
+def loads(data: bytes) -> Continuation[Any, Any]:
+    """Deserialize a continuation previously created by :func:`dumps`."""
+    value = pickle.loads(data)
+    return cast(Continuation[Any, Any], value)
